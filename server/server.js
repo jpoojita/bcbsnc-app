e9139b068d4f3b3cebd1efcc437ad7ec67d64bc4
DaisyJohnson <daisyjohnson07@protonmail.com>
2021-06-22 20:44:24 -0500

    Update server to perform post request to external url

--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var cors = require('cors');
+var app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.post('/upload', (req, res) => {
-  res.redirect('/');
+  res.send('Received a POST HTTP method');
+  async () => {
+    axios
+      .post('https://imagehasbeenverified.example.endpoint', data)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error(`HTTP error! status: ${res.status}`);
+        }
+        console.log(`Success with ${res}`);
+      })
+      .catch(err => {
+        console.log(`Failed with ${err}`);
+      });
+  };
 });
 
 const PORT = process.env.PORT || 4000;
